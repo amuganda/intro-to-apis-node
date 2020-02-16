9912c545dcc97c4669bf092df2de2f89fe3b06e7
Anthony <anthony.muganda@gmail.com>
2020-02-16 14:49:42 +0000

    Update app.js
    Complimentr App written with javascript framework

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const TwilioClient = require ("twilio")
 const app = express();
 const port = 3000;
 
+const client = new TwilioClient();
+const from = process.env.twilioPhoneNumber;
 // This is a single page application and it's all rendered in public/index.html
 app.use(express.static("public"));
 // Parse the body of requests automatically
@@ -11,17 +14,24 @@ app.use(bodyParser.json());
 
 app.get("/api/compliments", async (req, res) => {
   // TODO: Get a list of messages sent from a specific number
-  const sentMessages = [];
+  const sentMessages = await client.messages.list({from: twilioPhoneNumber});
   // TODO: Gather only the body of those messages for sending to the client
-  const compliments = [];
+  //From Twilio Documentation -- Fetch body of Message and output to console
+  //client.messages(message.body).fetch().then(message => console.log(message.to));
+  //From Tutorial - Use map function
+  const compliments = sentMessages.map(message => message.body);
   res.json(compliments);
 });
 
 app.post("/api/compliments", async (req, res) => {
   const to = req.body.to;
-  const from = process.env.TWILIO_PHONE_NUMBER;
+  const from = process.env.twilioPhoneNumber;
   const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers.referer}`;
   // TODO: Send a message
+  await client.messages.create({to, from, body});
+} catch (err) {
+  res.status(err.status).json({success: false, message: err.message });
+}
   res.json({ success: false });
 });
 
